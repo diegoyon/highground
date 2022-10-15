4dad05dac06eac345ebf4f7fa247424498f3bd20
Diego Yon <diegoyon@outlook.com>
2022-10-15 17:59:42 -0600

    Set up navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import './App.css';
+import Workouts from './pages/Workouts';
+import Leaderboard from './pages/Leaderboard';
 
 const App = () => {
   return (
@@ -9,6 +12,8 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
+        <Route path="/workouts" exact element={<Workouts />} />
+        <Route path="/leaderboard" exact element={<Leaderboard />} />
       </Routes>
     </Router>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <ul className="navbar">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/workouts">Workouts</NavLink>
+      <NavLink to="/leaderboard">Leaderboard</NavLink>
+    </ul>
+  );
+};
 
-export default Navbar
+export default Navbar;
+
