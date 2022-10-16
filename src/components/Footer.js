7cae7da087ea1ede8e1a1b67c3ba6587df1fa4ff
Diego Yon <diegoyon@outlook.com>
2022-10-16 15:01:50 -0600

    finish footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import logo from '../images/logo_blanco.png';
 import './Footer.css';
+import facebook from '../images/facebook.png';
+import instagram from '../images/instagram.png';
 
 const Footer = () => {
   return (
     <>
       <section className="footer">
         <ul className="social">
-          <li>instagram</li>
-          <li>facebook</li>
+          <li>
+            <img className="facebook" src={facebook} />
+          </li>
+          <li>
+            <img className="instagram" src={instagram} />
+          </li>
         </ul>
         <div className="logo-info">
           <img className="logo-footer" src={logo} />
+          <div className="brand">
+            <p>HIGH GROUND FITNESS COMPETITION 2022</p>
+            <p>PRESENTED BY CROSSFIT XELA</p>
+          </div>
         </div>
       </section>
       <section className="subfooter">
