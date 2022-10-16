91b2a707af2405376b17fb2f407b290048b64d3e
Diego Yon <diegoyon@outlook.com>
2022-10-15 19:28:55 -0600

    link google sheet to website

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -2,11 +2,37 @@ import React, { useState } from 'react';
 import './Leaderboard.css';
 
 const Leaderboard = () => {
-  const [category, setCategory] = useState();
+  const [category, setCategory] = useState('Intermedio Mujeres');
 
   const handleChange = (event) => {
     setCategory(event.target.value);
   };
+
+  let content;
+  switch (category) {
+    case 'Intermedio Mujeres':
+      content = (
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQOe1-D_UlByLk1jaRmJaI5jbzzsUJn2L7CoBc1ejDBTmXThHu8ksak7e22AhXQt8JTBd0qvfmnYCcT/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+      );
+      break;
+    case 'Intermedio Hombres':
+      content = (
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQOe1-D_UlByLk1jaRmJaI5jbzzsUJn2L7CoBc1ejDBTmXThHu8ksak7e22AhXQt8JTBd0qvfmnYCcT/pubhtml?gid=1678873583&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+      );
+      break;
+    case 'RX Mujeres':
+      content = (
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQOe1-D_UlByLk1jaRmJaI5jbzzsUJn2L7CoBc1ejDBTmXThHu8ksak7e22AhXQt8JTBd0qvfmnYCcT/pubhtml?gid=1621358835&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+      );
+      break;
+    case 'RX Hombres':
+      content = (
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQOe1-D_UlByLk1jaRmJaI5jbzzsUJn2L7CoBc1ejDBTmXThHu8ksak7e22AhXQt8JTBd0qvfmnYCcT/pubhtml?gid=1777000486&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+      );
+      break;
+    default:
+      break;
+  }
 
   return (
     <div className="leaderboard">
@@ -16,6 +42,7 @@ const Leaderboard = () => {
         <option value="RX Mujeres">RX Mujeres</option>
         <option value="RX Hombres">RX Hombres</option>
       </select>
+      <div className="table">{content}</div>
     </div>
   );
 };
