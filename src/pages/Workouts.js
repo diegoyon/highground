6594078185530a573f797e89b326e0aecfa5fdca
Diego Yon <diegoyon@outlook.com>
2022-11-09 14:34:23 -0600

    Add workout 7

--- src/pages/Workouts.js
+++ src/pages/Workouts.js
@@ -16,9 +16,9 @@ const Workouts = () => {
   switch (workout) {
     case 'WORKOUT 1':
       int_content =
-        'For time \n\n Buy in:\n 30 Burpee Box Get Overs\n\n 5 Rounds\n 7 Shoulder to overhead (95/65)\n 100ft Sled Drag';
+        'For time \n\n Buy in:\n 30 Burpee Box Get Overs\n\n 5 Rounds\n 7 Shoulder to overhead (95/65)\n 100ft Sled Drag\n\n TC: 13min';
       rx_content =
-        'For time \n\n Buy in:\n 30 Burpee Box Get Overs\n\n 5 Rounds\n 7 Shoulder to overhead (135/95)\n 100ft Sled Drag';
+        'For time \n\n Buy in:\n 30 Burpee Box Get Overs\n\n 5 Rounds\n 7 Shoulder to overhead (135/95)\n 100ft Sled Drag\n\n TC: 13min';
       break;
     case 'WORKOUT 2 & 3':
       int_content =
@@ -28,15 +28,15 @@ const Workouts = () => {
       break;
     case 'WORKOUT 4':
       int_content =
-        'Sprints \n 3 Elimination Rounds\n\n 3 Shuttle Runs\n 12/8 Toes To Bar\n3 Shuttle Runs\n';
+        'Sprints \n 3 Elimination Rounds\n\n 3 Shuttle Runs\n 12/8 Toes To Bar\n3 Shuttle Runs\n\n TC: 3min per round';
       rx_content =
-        'Sprints \n 3 Elimination Rounds\n\n 3 Shuttle Runs\n 8/5 Bar Muscle Ups\n3 Shuttle Runs\n';
+        'Sprints \n 3 Elimination Rounds\n\n 3 Shuttle Runs\n 8/5 Bar Muscle Ups\n3 Shuttle Runs\n\n TC: 3min per round';
       break;
     case 'WORKOUT 5':
       int_content =
-        "For Time \n50ft OH DB Walking Lunges (35's/25's) \n 35 Box Jump Over (24\"/20\")\n 25 Hand-release Push Ups\n 50ft DB Walking Lunges (35's/25's)\n 15 Devil Press (35's/25's)\n 50ft DB Walking Lunges (35's/25's)\n 25 Hand-release Push Ups\n 35 Box Jump Over (24\"/20\")\n 50ft OH DB Walking Lunges (35's/25's)\n";
+        "For Time \n50ft OH DB Walking Lunges (35's/25's) \n 35 Box Jump Over (24\"/20\")\n 25 Hand-release Push Ups\n 50ft DB Walking Lunges (35's/25's)\n 15 Devil Press (35's/25's)\n 50ft DB Walking Lunges (35's/25's)\n 25 Hand-release Push Ups\n 35 Box Jump Over (24\"/20\")\n 50ft OH DB Walking Lunges (35's/25's)\n\n TC: 17min";
       rx_content =
-        "For Time \n50ft OH DB Walking Lunges (50's/35's) \n 35 Box Jump Over (24\"/20\")\n 25 Handstand Push Ups\n 50ft DB Walking Lunges (50's/35's)\n 15 Devil Press (50's/35's)\n 50ft DB Walking Lunges (50's/35's)\n 25 Handstand Push Ups\n 35 Box Jump Over (24\"/20\")\n 50ft OH DB Walking Lunges (50's/35's)\n";
+        "For Time \n50ft OH DB Walking Lunges (50's/35's) \n 35 Box Jump Over (24\"/20\")\n 25 Handstand Push Ups\n 50ft DB Walking Lunges (50's/35's)\n 15 Devil Press (50's/35's)\n 50ft DB Walking Lunges (50's/35's)\n 25 Handstand Push Ups\n 35 Box Jump Over (24\"/20\")\n 50ft OH DB Walking Lunges (50's/35's)\n\n TC: 17min";
       break;
     case 'WORKOUT 6':
       int_content =
@@ -45,6 +45,10 @@ const Workouts = () => {
         'Snatch ladder \n\n(H) 145 - 155 - 165 - 175 - 185 - 195 - 205 - 215 - 225 - 235 \n (M) 95 - 105 - 115 - 125 - 130 - 135 - 140 - 145 - 150 - 155';
       break;
     case 'WORKOUT 7':
+      int_content =
+        'For Time \n\n 21 Chest-to-bar Pull Ups \n 21 Back Squats (155/105) \n 15 Chest-to-bar Pull Ups \n 15 Front Squats (155/105) \n 9 Chest-to-bar Pull Ups \n 9 Overhead Squats (155/105)\n\n TC: 8min';
+      rx_content =
+        'For Time \n\n 21 Pull Ups \n 21 Back Squats (115/85) \n 15 Pull Ups \n 15 Front Squats (115/85) \n 9 Pull Ups \n 9 Overhead Squats (115/85)\n\n TC: 8min';
       break;
     default:
       break;
